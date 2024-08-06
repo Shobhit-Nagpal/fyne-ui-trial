872ff41469a1c56df9badf5df211ec6573f6fd66
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-08-07 00:07:39 +0530

    we have to start event loop / run loop when app starts

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
@@ -8,7 +10,17 @@ import (
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
-
 	w.SetContent(widget.NewLabel("Hello World!"))
 	w.ShowAndRun()
+  tidyUp()
+}
+
+//For a GUI application to work it needs to run an event loop (sometimes called a runloop) 
+//that processes user interactions and drawing events.
+
+//In Fyne this is started using the App.Run() or Window.ShowAndRun() functions. 
+//One of these must be called from the end of your setup code in the main() function.
+
+func tidyUp() {
+	fmt.Println("Exited")
 }
